1c06c859dd9f41ce7e99a621f328f8b900b9a906
Maxime Verdy <maximeverdy@gmail.com>
2020-10-25 17:59:38 +0100

    TOP500 selon Rolling Stone et TOP124 selon maxime

--- backend/bdd/SchemaTop50.js
+++ backend/bdd/SchemaTop50.js
@@ -7,6 +7,21 @@ var top50Schema = mongoose.Schema({
     titre: String,
 })
 
+// var top500Schema = mongoose.Schema({
+//     classement: Number,
+//     artiste: String,
+//     titre: String,
+//     annee: Number,
+// })
+
+// var top124Schema = mongoose.Schema({
+//     artist: String,
+//     title: String,
+//     style: String,
+// })
+
 var top50Model = mongoose.model('top50', top50Schema)
+// var top50Model = mongoose.model('TOP500', top500Schema)
+// var top50Model = mongoose.model('TOP124maxime', top124Schema)
 
 module.exports = top50Model;
