664c82f793422dace6f21d57332f59135c32bc66
Florianne Nollet <florianne-n@hotmail.fr>
2020-10-26 18:23:45 +0100

    ajout de page navigation

--- front/App.js
+++ front/App.js
@@ -4,10 +4,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import * as Font from 'expo-font';
 import {AppLoading} from 'expo';
-import Carousel from './screen/carousel';
+import {createAppContainer } from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+// import Carousel from './screen/carousel';
 import HomeInvite from './screen/homeinvite';
 import Onboarding from './screen/onboarding';
-
+import SignIn from './screen/SignIn';
 
 
 const getFonts = () => Font.loadAsync({
@@ -16,21 +19,44 @@ const getFonts = () => Font.loadAsync({
     'Roboto-Bold': require('./assets/fonts/Roboto/Roboto-Bold.ttf'),
   })
 
-export default function App() {
+  var StackNavigator = createStackNavigator({
+    Onboarding:  Onboarding,  
+    // ExempleB:  ExempleScreenB,
+  },
+  {headerMode: 'none'}
+  );
+
+  const Navigation = createAppContainer(StackNavigator);
+
+ export default function App() {
 
   const [ fontsLoaded, setFontsLoaded ] = useState(false);
 
+
   if(fontsLoaded){
     return (
-      <HomeInvite/>
+      //<HomeInvite/>
      //<Onboarding/>
+     <Navigation/>
     )
   } else {
       return (
-        <AppLoading
-          startAsync={getFonts}
-          onFinish={()=> setFontsLoaded(true)}
-        />
+        // <AppLoading
+        //   startAsync={getFonts}
+        //   onFinish={()=> setFontsLoaded(true)}
+        // />
+        <SignIn></SignIn>
       )
     } 
   }
+
+
+
+  // pour la navigation via StackNavigator, coller ce genre de lien dans les screens
+// function ExempleScreenA(props){
+//   return (
+//     <View> <Button title="Go to page B"
+//      onPress={() => props.navigation.navigate('ScreenB')} />
+//     </View>
+//   );
+// }
