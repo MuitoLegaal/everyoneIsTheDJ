0d577c2aab87f2f46d8da3361e79914dd1f1ce9f
MuitoLegaal <62960639+MuitoLegaal@users.noreply.github.com>
2020-10-27 09:59:27 +0100

    debut de jour2

--- front/App.js
+++ front/App.js
@@ -9,7 +9,7 @@ import {createStackNavigator} from 'react-navigation-stack';
 
 // import Carousel from './screen/carousel';
 import SignIn from './screens/SignIn'; // attention c'est ./screen avec un "s"
-import SignUp from './screens/signUp'; // attention c'est ./screen avec un "s"
+import SignUp from './screens/SignUp'; // attention c'est ./screen avec un "s"
 import HomeInvite from './screens/homeinvite';
 import Onboarding from './screens/onboarding';
 import DJhoteFirstScreen from './screens/DJhoteFirstScreen';
