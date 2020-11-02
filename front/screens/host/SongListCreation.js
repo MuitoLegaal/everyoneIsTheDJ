3bacd571d5f171a1aa85987aa9aead7f5a85c404
Florianne Nollet <florianne-n@hotmail.fr>
2020-11-02 17:57:11 +0100

    pull soir 2 nov

--- front/screens/host/SongListCreation.js
+++ front/screens/host/SongListCreation.js
@@ -19,14 +19,15 @@ function SongListCreation(props) {
   const [titreProposeHote, setTitreProposeHote] = useState();
   const [titreListHote, setTitreListHote] = useState([]);
   const [TOPlist, setTOPlist] = useState([]);
+  const [errorArtist, setErrorArtist] = useState();
 
   let artistUndefined;
-  let listHote;
+
 
   useEffect(() => {
     const findTOP = async() => {
       // ----------------------------------------- METTRE A JOUR l'IP --------------------------------------------
-      const TOPdata = await fetch('http://172.17.1.106:3000/findTOP', {
+      const TOPdata = await fetch('http://172.17.1.32:3000/findTOP', {
       })
       var TOP = await TOPdata.json();
       setTOPlist(TOP) 
@@ -41,7 +42,7 @@ function SongListCreation(props) {
   var handleAjouterTitre = async () => {
 
     //APPEL AU BACKEND//
-    var rawResponse = await fetch('http://192.168.0.20:3000/ajoutertitre', {
+    var rawResponse = await fetch('http://172.17.1.32:3000/ajoutertitre', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: `titreFromFront=${titreProposeHote}`
@@ -53,24 +54,33 @@ function SongListCreation(props) {
 
     //FRONT//
     console.log('titreProposeHote++++++++++++++++', titreProposeHote)
-    setTitreListHote([...titreListHote, titreProposeHote])
-    if (titreProposeHote != null) {
-      console.log("titreProposeHoteTTTTTTTTTTTT", titreProposeHote)
-      listHote = titreListHote.map((titre, i) => {
-        return (
-          <View style={{ flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'space-between', marginRight: '2%', }}>
-            key={i}
-            <Text style={styles.songtextAjout}>{titre}</Text>
-            <FontAwesomeIcon onPress={()=> handleSupprimerTitre()} icon={faTrash} size={20} style={{ color: "#fff" }} />
-          </View>
-        )
-      }
-      );
-      setTitreProposeHote('')
-    }
-    else {
+    
+    // setTitreListHote([...titreListHote, titreProposeHote])
+    // if (titreProposeHote != undefined) {
+    //   console.log("titreProposeHoteTTTTTTTTTTTT", titreProposeHote)
+      
+    //   listHote = titreListHote.map((titre, i) => {
+    //     return (
+    //       <View style={{ flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'space-between', marginRight: '2%', }}>
+    //         key={i}
+    //         <Text style={styles.songtextAjout}>{titre}</Text>
+    //         <FontAwesomeIcon onPress={()=> handleSupprimerTitre()} icon={faTrash} size={20} style={{ color: "#fff" }} />
+    //       </View>
+    //     )
+    //   }
+    //   );
+    //   setTitreProposeHote('')
+    // }
+    // else {
       console.log("titre proposé ========= ", titreProposeHote)
-      artistUndefined = <Badge status="error" badgeStyle={{ color: 'white', backgroundColor: '#FF0060' }} value="Le champ est vide"></Badge>
+      // artistUndefined = <Badge status="error" badgeStyle={{ color: 'white', backgroundColor: '#FF0060' }} value="Le champ est vide"></Badge>
+    // }
+    if(titreProposeHote === undefined){
+        setErrorArtist(<Badge status="error" badgeStyle={{ color: 'white', backgroundColor: '#FF0060' }} value="Le champ est vide"></Badge>)
+    } else {
+       setErrorArtist()
+       setTitreListHote([...titreListHote, titreProposeHote])
+       
     }
   }
 
@@ -79,7 +89,7 @@ function SongListCreation(props) {
 
     var idTest = "5f9d9e1aa3eb5025a0a807ed"
 
-    var rawResponse = await fetch('http://172.17.1.106:3000/supprimertitre', {
+    var rawResponse = await fetch('http://172.17.1.32:3000/supprimertitre', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: `titreIdFromFront=${idTest}`
@@ -92,15 +102,17 @@ function SongListCreation(props) {
 
 
 
+  var listHote = titreListHote.map((titre, i) => {
+        return (
+          <View style={styles.titre}>
+          <FontAwesomeIcon onPress={()=> handleSupprimerTitre()} icon={faTrash} size={20} style={{color: "#fff", marginLeft: '2%'}} />
+          <Text style={styles.songtext}>{titre}</Text>
+          </View>
+        )
+      }
+      );
 
 
-
-  // GESTION DES ERREUR A REVOIR
-
-  // var titreAleatoire = 
-  // return (
-  //    <Text style={styles.songtext}>Shakira - Waka waka</Text>
-  // )
 
 
   return (
@@ -125,16 +137,12 @@ function SongListCreation(props) {
                       <Text style={styles.text}>Bienvenu dans la soirée de </Text>
                       <Text style={styles.subtitle} >NOM DE LEVENT {props.nameToDisplay}</Text>
                       <Text style={styles.bodytext}>Compose ta liste de titres candidats aux votes (3 titres minimum).</Text>
-                      {artistUndefined}
+                      
+                      {errorArtist}
 
                         {TOPlist!=0 && (   
                           
                           <View style={{flex:1, flexDirection: 'column'}}>
-                                {/* <View style={{flexDirection:'row', alignItems:'flex-end', justifyContent:'space-between', marginRight:'2%', marginBottom:'5%'}}>
-                                      {titreAleatoire}
-                                      <Text style={styles.songtext}>Shakira - Waka waka</Text>
-                                      <FontAwesomeIcon onPress={()=> handleSupprimerTitre()} icon={faTrash} size={20} style={{color: "#fff"}} />
-                                </View> */}
                             <View style={styles.titre}>
                                   <FontAwesomeIcon onPress={()=> handleSupprimerTitre()} icon={faTrash} size={20} style={{color: "#fff", marginLeft: '2%'}} />
                                   <Text style={styles.songtext}>{TOPlist.randomTitles.title1}</Text>
@@ -155,13 +163,14 @@ function SongListCreation(props) {
                                   <FontAwesomeIcon onPress={()=> handleSupprimerTitre()} icon={faTrash} size={20} style={{color: "#fff", marginLeft: '2%'}} />
                                   <Text style={styles.songtext}>{TOPlist.randomTitles.title5}</Text>
                             </View>
-                          {/* </View> */}
+                            
+                            {listHote}
                         </View>
                         )}
 
                             {/* <View style={{flexDirection:'column'}}>
                                   {listHote}
-                            </View> */}
+                            </View>  */}
 
                     
                   </View>
@@ -259,122 +268,7 @@ function SongListCreation(props) {
                         </View> */}
           </View>
 
-        {/* <View>
-          {artistUndefined}
-          <View style={{ alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between', marginTop: '5%' }}>
-            <Input
-              label='Artiste - Titre :'
-              placeholder='Lady Gaga - Poker Face'
-              type='text'
-              containerStyle={{
-                color: '#fff',
-                width: 290,
-                marginTop: '3%',
-                alignItems: 'stretch'
-
-              }}
-              inputStyle={{
-                fontFamily: 'Roboto-Bold',
-                fontSize: 16,
-                color: '#fff',
-
-              }}
-              labelStyle={{
-                fontFamily: 'Roboto-Bold',
-                fontSize: 18,
-                color: '#584DAD',
-
-              }}
-              onChangeText={text => setTitreProposeHote(text)}
-              value={titreProposeHote}
-
-
-            />
-            <Button
-              title='+'
-              buttonStyle={{
-                backgroundColor: '#584DAD',
-                borderRadius: 10,
-                width: 40,
-                height: 40,
-                color: 'white',
-                marginRight: '2%'
-
-              }}
-              onPress={() => handleAjouterTitre()}
-
-            />
-          </View>
-
-          <View style={{ alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between' }}>
-            <View style={{ alignItems: 'center', flexDirection: 'column', borderBottomColor: "grey", borderBottomWidth: 1, marginTop: '5%' }}>
-              <Text style={{ fontFamily: 'Roboto-Bold', fontSize: 18, color: '#584DAD', marginLeft: '3%' }}>Artiste - Titre aléatoire : </Text>
-              <Text style={{ fontFamily: 'Roboto-Bold', fontSize: 16, color: '#fff', marginBottom: '2%' }}> %TitresBdd% </Text>
-            </View>
-            <Button
-              title=''
-
-              buttonStyle={{
-                backgroundColor: '#584DAD',
-                borderRadius: 10,
-                width: 40,
-                height: 40,
-                color: 'white',
-
-
-
-              }}
-              icon={<FontAwesomeIcon
-                icon={faServer}
-                size={15}
-                color="white"
-              />
-              }
-              onPress={() => handleAjouterTitre()}
-
-            />
-            <Button
-              title='+'
-              buttonStyle={{
-                backgroundColor: '#584DAD',
-                borderRadius: 10,
-                width: 40,
-                height: 40,
-                color: 'white',
-                marginRight: '2%'
-
-              }}
-              onPress={() => handleAjouterTitre()}
-
-            />
-          </View> */}
-
-          {/* <Button 
-                      title=" Rafraîchir la liste" 
-                      //onPress={() => props.navigation.navigate('EventCreation')}
-                      //onPress={() => props.navigation.navigate('TimerConfigFIRST')}
-                      buttonStyle={{
-                            backgroundColor: '#E59622',
-                            marginTop: '10%',
-                            marginBottom: '10%',
-                            borderRadius: 10,
-                            }}
-                      icon={<FontAwesomeIcon
-                              icon={faServer}
-                              size={15}
-                              color="white"
-                            />
-                              }
-                            
-                    />  */}
-
-        {/* </View> */}
-
-
-        {/* ------------FLECHE DE RETOUR DEFINIE ICI EN DESSOUS (supprimer tout le bouton) ------------- */}
-        {/* <Button buttonStyle={{paddingTop: 15}} title="<- flèche retour en haut" 
-                        onPress={() => props.navigation.navigate('EventCreation')}
-                  ></Button>  */}
+      
 
             </KeyboardAwareScrollView>
             
@@ -414,7 +308,7 @@ function mapStateToProps(state){
       wrap: {
           flexDirection: 'column',
           textAlign: 'center',
-          height: hp('100%'), // 70% of height device screen
+          //height: hp('100%'), // 70% of height device screen
           width: wp('100%'),  // 80% of width device screen 
           backgroundColor: '#131313',
           borderTopWidth:1,
